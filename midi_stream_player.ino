fa8c356cfa02c0552631d3099c635a90c81d5f27
Marcel Licence <marcel.licence@gmx.de>
2022-05-28 17:53:51 +0200

    Update file header

--- midi_stream_player.ino
+++ midi_stream_player.ino
@@ -1,13 +1,45 @@
 /*
  * Copyright (c) 2022 Marcel Licence
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Dieses Programm ist Freie Software: Sie können es unter den Bedingungen
+ * der GNU General Public License, wie von der Free Software Foundation,
+ * Version 3 der Lizenz oder (nach Ihrer Wahl) jeder neueren
+ * veröffentlichten Version, weiter verteilen und/oder modifizieren.
+ *
+ * Dieses Programm wird in der Hoffnung bereitgestellt, dass es nützlich sein wird, jedoch
+ * OHNE JEDE GEWÄHR,; sogar ohne die implizite
+ * Gewähr der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+ * Siehe die GNU General Public License für weitere Einzelheiten.
+ *
+ * Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+ * Programm erhalten haben. Wenn nicht, siehe <https://www.gnu.org/licenses/>.
  */
 
-/*
- * sd_module.ino
+/**
+ * @file midi_stream_player.ino
+ * @author Marcel Licence
+ * @date 22.05.2021
  *
- *  Created on: 22.05.2022
- *      Author: PC
+ * @brief This file contains code to access the midi file stream player
+ *
+ * The midi file stream player will be part of the ML_SynthTools library
+ * This file is required to define all file accessing functions which are not part of the library
+ * In addition to that they are dependend to the different platform you are using
  */
+
 
 #ifdef __CDT_PARSER__
 #include <cdt.h>
@@ -30,8 +62,8 @@ extern SdFatFs fatFs;
 #define FST fs::FS
 #endif
 
-#define MIDI_FS_LITTLE_FS	0
-#define MIDI_FS_SD_MMC	1
+#define MIDI_FS_LITTLE_FS   0
+#define MIDI_FS_SD_MMC  1
 
 
 #define FORMAT_LITTLEFS_IF_FAILED true
